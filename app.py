37b3e4666eccdf72c1a58aee2434eb1c7202e7b8
MarwanWalid2 <18085@stemoegypt.edu.eg>
2023-10-21 20:11:42 -0400

    created shelters and add pets

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, flash, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from bson import ObjectId
 from dotenv import load_dotenv
@@ -12,11 +12,17 @@ app.secret_key = os.urandom(24)
 
 # Connect to MongoDB
 # Connect to MongoDB Atlas
-client = MongoClient(os.environ.get('MONGO_URI'))
-db = client[os.environ.get('MONGO_DBNAME')]  
+
+
+client = MongoClient ('mongodb+srv://admi:sSq4KE8jp6DgusOD@cluster0.35ye0fk.mongodb.net')
+db = client ['SWE']
+
+# client = MongoClient(os.environ.get('MONGO_URI'))
+# db = client[os.environ.get('MONGO_DBNAME')]  
 
 pets = db["pets"]
 humans = db["humans"]
+shelters = db["shelters"]
 
 pets.create_index([
     ("breed", "text"),
@@ -47,6 +53,19 @@ def index():
         return redirect(url_for('login'))
     return render_template('index.html', user_doc=user_doc)
 
+@app.route('/index_shelter')
+def index_shelter():
+    user_id = session.get('user_id')
+    if not user_id:
+        # Not logged in, redirect to login page
+        return redirect(url_for('login'))
+
+    user_doc = shelters.find_one({"_id": ObjectId(user_id)})
+    if not user_doc:
+        # User not found, clear the session and redirect to login
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('index_shelter.html', user_doc=user_doc)
 
 
 
@@ -57,10 +76,18 @@ def edit(user_id):
 
 @app.route('/edit/<user_id>', methods=['POST'])
 def edit_user(user_id):
-    # username = ObjectId(request.form.get('username'))
+    # Get the new values from the form
+    username = request.form.get('username')
     passw = request.form.get('passw')
     
-    humans.update_one({"_id": ObjectId(user_id)}, {"$set": {"passw": passw}})
+    # Update the database with the new values
+    update_data = {}
+    if username:
+        update_data["username"] = username
+    if passw:
+        update_data["passw"] = passw
+    
+    humans.update_one({"_id": ObjectId(user_id)}, {"$set": update_data})
     
     return redirect(url_for('index'))
 
@@ -71,16 +98,27 @@ def login():
         username = request.form.get('username')
         passw = request.form.get('passw')
 
-        # Check credentials
+        # Check credentials for humans
         user = humans.find_one({"username": username, "passw": passw})
+        shelter = shelters.find_one({"username": username, "passw": passw})
+        
         if user:
-            # Credentials are correct
+            # Credentials are correct for a human
             session['user_id'] = str(ObjectId(user['_id']))
             return redirect(url_for('index'))
-        else:
-            # Invalid credentials
-            return render_template('login.html', error="Invalid username or password")
+
+        # Check credentials for shelters
+      
+        if shelter:
+            # Credentials are correct for a shelter
+            session['user_id'] = str(ObjectId(shelter['_id']))
+            return redirect(url_for('index_shelter'))
+
+        # Invalid credentials
+        return render_template('login.html', error="Invalid username or password")
+    
     return render_template('login.html')
+
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -126,12 +164,65 @@ def add():
             return redirect(url_for('index'))
     return render_template('add.html')
 
+
+@app.route('/add_pets', methods=['GET','POST'])
+def add_pets():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        age = request.form.get('age')
+        breed = request.form.get('breed')
+        type = request.form.get('type')
+
+        # Create New pet
+        pets.insert_one({
+                "name": name,
+                "age": age,
+                "breed": breed,
+                "type": type,
+                })
+        return redirect(url_for('index_shelter'))
+    return render_template('add_pets.html')
+
+
+@app.route('/delete_account')
+def delete_account():
+    user_id = session.get('user_id')
+    if not user_id:
+        # Not logged in, redirect to login page
+        return redirect(url_for('login.html'))
+
+    # Render a confirmation page before actual deletion
+    return render_template('delete_account.html')
+
+@app.route('/confirm_delete', methods=['POST'])
+def confirm_delete():
+    user_id = session.get('user_id')
+    if not user_id:
+        # Not logged in, redirect to login page
+        return redirect(url_for('login.html'))
+
+    try:
+        # Actually delete the user's data from the database
+        humans.delete_one({"_id": ObjectId(user_id)})
+
+        # Clear the user session and other data
+        session.pop('user_id', None)
+        # if you stored anything else in session, clear it here
+
+        flash('Your account has been deleted.', 'success')
+        return redirect(url_for('landing.html'))  # or wherever you want to redirect after deletion
+
+    except Exception as e:
+        # Log the error for debugging
+        print(str(e))
+        flash('There was a problem deleting your account.', 'error')
+        return redirect(url_for('delete_account'))
+
 @app.route('/like/<pet_id>')
 def like_pet(pet_id):
     user_id = session.get('user_id')
     humans.update_one({"_id": ObjectId(user_id)}, {"$addToSet": {"likedPets": ObjectId(pet_id)}})
     return redirect(url_for('search'))
-
 
 @app.route('/liked', methods=['GET'])
 def liked_pets():
